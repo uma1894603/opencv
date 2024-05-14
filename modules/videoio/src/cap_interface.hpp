d7d1a3ed3442a4476ad8616ae2152cefb5240700
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2024-05-14 13:43:33 +0300

    initial commit
    FFmpeg test

--- modules/videoio/src/cap_interface.hpp
+++ modules/videoio/src/cap_interface.hpp
@@ -409,6 +409,7 @@ public:
 //==================================================================================================
 
 Ptr<IVideoCapture> cvCreateFileCapture_FFMPEG_proxy(const std::string &filename, const VideoCaptureParameters& params);
+Ptr<IVideoCapture> cvCreateBufferCapture_FFMPEG_proxy(const std::vector<uchar> &buffer, const VideoCaptureParameters& params);
 Ptr<IVideoWriter> cvCreateVideoWriter_FFMPEG_proxy(const std::string& filename, int fourcc,
                                                    double fps, const Size& frameSize,
                                                    const VideoWriterParameters& params);
