8e590223bb639a8d3cd4ccd3a0321ca7a4f17a04
Alexander Smorkalov <alexander.smorkalov@xperience.ai>
2024-10-10 14:48:35 +0300

    Code review fixes.

--- cmake/OpenCVFindWebP.cmake
+++ cmake/OpenCVFindWebP.cmake
@@ -27,26 +27,6 @@ else()
     INCLUDE(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
     FIND_PACKAGE_HANDLE_STANDARD_ARGS(WebP DEFAULT_MSG WEBP_LIBRARY WEBP_INCLUDE_DIR)
 
-    SET(WEBP_LIBRARIES ${WEBP_LIBRARY})
-
-    if(WEBP_MUX_LIBRARY)
-        SET(WEBP_LIBRARIES ${WEBP_LIBRARIES} ${WEBP_MUX_LIBRARY} ${WEBP_DEMUX_LIBRARY})
-        SET(WEBP_INCLUDE_DIRS ${WEBP_INCLUDE_DIR})
-
-        try_compile(VALID_WEBP
-        "${OpenCV_BINARY_DIR}"
-        "${OpenCV_SOURCE_DIR}/cmake/checks/webp_test.cpp"
-        CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${WEBP_INCLUDE_DIR}"
-        LINK_LIBRARIES ${WEBP_LIBRARIES}
-        OUTPUT_VARIABLE OUTPUT
-        )
-
-        if(VALID_WEBP)
-             message(STATUS "Proper version of WebP library found.")
-        else()
-            set(WEBP_FOUND 0)
-            set(HAVE_WEBP 0)
-            message(STATUS "Proper version of WebP library not found. You can choose built from source option.")
-        endif()
-    endif()
+    SET(WEBP_LIBRARIES ${WEBP_LIBRARY} ${WEBP_MUX_LIBRARY} ${WEBP_DEMUX_LIBRARY})
+    SET(WEBP_INCLUDE_DIRS ${WEBP_INCLUDE_DIR})
 endif()
